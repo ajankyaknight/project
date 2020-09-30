0ffc768ebee0c56813e0c7f685565fc0632952e9
Ravi Goswami <zneroravi@gmail.com>
2020-10-01 01:17:34 +0530

    update
    update and fix issue

--- idelete.php
+++ idelete.php
@@ -1,26 +1,39 @@
-<?php
-$rcno = $_GET['rcno'];
+<?php 
+if (isset($_POST['submit'])) {
 
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="rtosystem";
+$name= $_POST['name'];
+$email= $_POST['email'];
+$mobile= $_POST['mobile'];
+$pass=$_POST['pass'];
+$repass=$_POST['repass'];
+$security=$_POST['security'];
 
-$conn=new mysqli($servername,$username,$password,$dbname);
-if($conn->connect_error)
+if ($pass != $repass) {
+	echo "password not match";
+} 
+if ($security != '56789') {
+	echo "security code wrong";
+}
+
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "rtosystem";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) 
 {
-die('connection failed:'.$conn->connect_error);
+	die("connection failed: ".$conn->connect_error);
 }
  echo "connect successfully";
 
-$sql="DELETE FROM insurance WHERE rcno='$rcno'";
+$sql="INSERT INTO registration (name, email, mobile, pass) VALUES ('$name','$email','$mobile','$pass')";
 if($conn->query($sql)===TRUE)
 {
-	echo"record deleted successfully";
-	header('Refresh: 2; URL=insurance.php');
- }
-else{
-	echo"error deleting record:".$conn->error;
+	echo"data inserted";
+	header('Refresh: 2; URL=login.html'); 
 }
-$conn->close();
+
+}
 ?>
